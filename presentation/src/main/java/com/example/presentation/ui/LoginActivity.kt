31e2686ce816c9f278c1581b41f7f9ff5a90cc42
chaewonkim <kch9870@naver.com>
2024-09-01 16:56:19 +0900

    #18 로그인 기능 구현
    -. 로그인 화면 구현
    -. 스플래쉬 화면 후 메인화면이 아닌 로그인 화면 이동
    -. 공통 ui (공통 버튼, underlineText) 추가
    -. AndroidManifest.xml LoginActivity 추가

---
+++ presentation/src/main/java/com/example/presentation/ui/LoginActivity.kt
@@ -0,0 +1,58 @@
+package com.example.presentation.ui
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.example.presentation.ui.login.LoginScreen
+import com.example.presentation.ui.theme.FitLogTheme
+import com.example.presentation.viewmodel.LoginViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class LoginActivity : AppCompatActivity() {
+    private val viewModel by viewModels<LoginViewModel>()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            FitLogTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    LoginScreen(
+                        clickLogin = { clickLogin() },
+                        clickGoogleLogin = { clickGoogleLogin() },
+                        clickSignUp = { clickSignUp() })
+                }
+            }
+        }
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    /**
+     * 로그인
+     */
+    fun clickLogin() {
+
+    }
+
+    /**
+     * 구글 로그인
+     */
+    fun clickGoogleLogin() {
+
+    }
+
+    /**
+     * 회원가입 창 이동
+     */
+    fun clickSignUp() {
+
+    }
+}
