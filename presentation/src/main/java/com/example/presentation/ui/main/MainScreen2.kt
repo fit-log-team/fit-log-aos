779676c2136af9ac4cd5b5d7a35827023aea8934
jangyeongwoo <sbtmxhs@gmail.com>
2024-09-01 17:44:20 +0900

    #18 메인화면  - 검색 ModalBottomSheet 초기작업

--- presentation/src/main/java/com/example/presentation/ui/main/MainScreen2.kt
+++ presentation/src/main/java/com/example/presentation/ui/main/MainScreen2.kt
@@ -1,32 +1,47 @@
 package com.example.presentation.ui.main
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.rounded.FavoriteBorder
 import androidx.compose.material.icons.rounded.Star
 import androidx.compose.material.icons.sharp.List
 import androidx.compose.material.icons.sharp.Person
+import androidx.compose.material3.BottomSheetDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.NavigationDrawerItemDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.rememberDrawerState
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
@@ -37,11 +52,10 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.presentation.ui.component.NavigatorMenu
 import com.example.presentation.ui.component.SearchBox
-import com.example.presentation.util.PermissionUtils
 import com.example.presentation.viewmodel.MainViewModel
 import kotlinx.coroutines.launch
-import timber.log.Timber
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen2(viewModel: MainViewModel = hiltViewModel(), tMapView: @Composable () -> Unit) {
     val items = listOf(
@@ -54,6 +68,13 @@ fun MainScreen2(viewModel: MainViewModel = hiltViewModel(), tMapView: @Composabl
     val scope = rememberCoroutineScope()
     val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
     var selectedItem by remember { mutableStateOf(items[0]) }
+
+    var showBottomSheet by remember { mutableStateOf(false) }
+    val sheetState = rememberModalBottomSheetState(
+        skipPartiallyExpanded = true,
+    )
+
+    var keyword by remember { mutableStateOf("") }
 
     ModalNavigationDrawer(
         drawerState = drawerState,
@@ -118,8 +139,59 @@ fun MainScreen2(viewModel: MainViewModel = hiltViewModel(), tMapView: @Composabl
                 }
             },
             clickSearch = {
-                //_ TODO 검색 버튼 클릭
+                scope.launch {
+                    showBottomSheet = true
+                }
             }
         )
+
+        if (showBottomSheet) {
+            keyword = ""
+            ModalBottomSheet(
+                sheetState = sheetState,
+                onDismissRequest = {
+                    showBottomSheet = false
+                },
+                shape = BottomSheetDefaults.HiddenShape,
+                dragHandle = {}
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .padding(top = 33.dp)
+                            .fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        IconButton(onClick = {
+                            showBottomSheet = false
+                        }) {
+                            Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "ArrowBack")
+                        }
+
+                        TextField(
+                            modifier = Modifier.background(MaterialTheme.colorScheme.primaryContainer),
+                            placeholder = {
+                                Text(text = "오늘의 걷기 목표는?")
+                            },
+                            value = keyword, onValueChange = {
+                                keyword = it
+                                //_ TODO poi를 조회할건데 최종 입력된 키워드로만 조회하도록 해야함. (이전 조회는 취소 해야함.)
+                            })
+
+                        IconButton(onClick = {
+                            keyword = ""
+                        }) {
+                            Icon(imageVector = Icons.Filled.Clear, contentDescription = "Clear")
+                        }
+                    }
+                    Divider()
+                }
+
+            }
+        }
+
     }
 }
